b790d9164259118f82ebcdf9032e95bc86a983f9
asantiba <alexis.santibanez.14@sansano.usm.cl>
2018-10-13 15:47:12 -0300

    Envio de mail con html

--- routes/index.js
+++ routes/index.js
@@ -24,43 +24,64 @@ module.exports = function(app,res){
 
     //Envia el mail a todos los correos de la bd
     app.post('/enviar_mail',function(req, res) {
-        var correos = ['canespfam@gmail.com','acamposa2874@gmail.com','alexis.santibanez.14@sansano.usm.cl'];
-        /*usuarioModel.get_mails(function(error, data) {
+        var text_html = '<html><head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252"><title>Print</title></head>'+
+                    '<body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0">'+
+                    '<img src="cid:unique@kreata.ee" width="1000" height="1815" alt=""><div style="position:absolute; left:10px; top: 1820px; background-color:#FFFFFF; outline-style:double; padding:5px">'+
+                    '<dl><dt>Format: PNG-24</dt><dt>Dimensions: 1000w x 1815h</dt><dt>Size: 527,1K</dt><dt>Settings: Non-Interlaced, Transparency on</dt></dl>'+
+                    '<pre><code>&lt;html&gt;&lt;head&gt;&lt;title&gt;Print&lt;/title&gt;&lt;meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"&gt;'+
+                    '&lt;/head&gt;&lt;body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0"&gt;&lt;img src="cid:unique2@kreata.ee" width="1000" height="1815" alt=""&gt;'+
+                    '&lt;/body&gt;&lt;/html&gt;</code></pre></div></body></html>';
+        /*
+        usuarioModel.get_mails(function(error, data) {
             if(error){
                 console.log(error.message);
             }else{
                 correos = data;
             }
-        });
-        */
+        }); */
+        var correos = ['canespfam@gmail.com','acamposa2874@gmail.com','alexis.santibanez.14@sansano.usm.cl'];
+        console.log('Cantidad de correos: ' + correos.length);
+        var correos_enviados = [];
+        var count = 0;
         for (var i = 0; i < correos.length; i++) {
+            console.log('Enviar email a: ' + correos[i] + '\n');
             var mailOptions = {
-                from: 'Jump',
+                from: 'no-reply@gojump.cl',
                 to: correos[i],
                 subject: 'Mail masivo jump',
-                text: 'Hola, te han invitado a un correo de prueba'
+                text: 'Hola, te han invitado a un correo de prueba',
+                html: text_html,
+                attachments: [{
+                    filename: 'WEB-copia.png',
+                    path: './public/HTML/01_files/WEB-copia.png',
+                    cid: 'unique@kreata.ee'
+                },
+                {
+                    filename: 'WEB-copia.png',
+                    path: './public/HTML/01_files/WEB-copia.png',
+                    cid: 'unique2@kreata.ee'
+                }]
             };
             transporter.sendMail(mailOptions, function(error, info){
                 if(error){
                     console.log(error);
-                    res.send("mala,\n %s",error.toString());
+                    count = count + 1;
+                    if (count == correos.length) {
+                        res.send('Error, no se han enviado emails');
+                    }
                 }
                 else{
-                    console.log('Email enviado a'+correos[i]);
-                    res.send("wena\n");
+                    correos_enviados.push(correos[count]);
+                    console.log('Email enviado a ' + correos[count] + ' ' + count);
+                    count = count + 1;
+                    if (count == correos.length) {
+                        res.send('Email enviado a: ' + correos_enviados + '\n');
+                    }
                 }
             });
             sleep(3000);
         }
 
-        transporter.sendMail(mailOptions, function(error, info){
-            if(error){
-                console.log(error);
-            } 
-            else{
-                console.log('Email enviado a'+correos[i]);
-            }
-        });
     });
     app.get('/enviar_especifico',function(req, res) {
             var mailOptions = {
