659be42a737aa2b3925d8d73b9a78e2e3a66e7d3
Japsz <benjamin.meneses.14@sansano.usm.cl>
2018-10-13 00:11:09 -0300

    algunas cosas extras
    - cambie el sender
    - una ruta pa mandar específico

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,10 @@ var usuarioModel =  require('../models/usuario');
 
 //Direccion origen para enviar mail
 var transporter = nodemailer.createTransport('SMTP',{
-    service: 'Gmail',
     auth: {
         //Este es el correo que utilizan para enviar mensajes y su contraseña, este correo lo cree yo
-        user: 'decidecorrectly@gmail.com',
-        pass: 'decidecorrectlyadsw'
+        user: 'no-reply@gojump.cl',
+        pass: 'belitajump'
     }
 });
 
@@ -48,4 +47,23 @@ module.exports = function(app,res){
             });
         }
     });
+    app.get('/enviar_especifico/:correo',function(req, res) {
+            var mailOptions = {
+                from: 'Jump',
+                to: req.params.correo,
+                subject: 'Mail masivo jump',
+                text: 'Hola, te han invitado a un correo de prueba'
+            };
+            transporter.sendMail(mailOptions, function(error, info){
+                if(error){
+                    console.log(error);
+                    res.send("mala,\n %s",error.toString());
+                }
+                else{
+                    console.log('Email enviado a'+correos[i]);
+                    res.send("wena\n");
+                }
+            });
+    });
+
 };
