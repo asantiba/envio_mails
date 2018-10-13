d139c854562b4f1de56efbd3ab773eb4d897c341
Japsz <benjamin.meneses.14@sansano.usm.cl>
2018-10-13 01:23:09 -0300

    Ojo que actualizé nodemailer

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ var nodemailer = require('nodemailer');
 var usuarioModel =  require('../models/usuario');
 
 //Direccion origen para enviar mail
-var transporter = nodemailer.createTransport('SMTP',{
+var transporter = nodemailer.createTransport({
+    host:'renes10416me.dedicados.cl',
+    port: 465,
     auth: {
         //Este es el correo que utilizan para enviar mensajes y su contraseña, este correo lo cree yo
         user: 'no-reply@gojump.cl',
@@ -40,9 +42,11 @@ module.exports = function(app,res){
             transporter.sendMail(mailOptions, function(error, info){
                 if(error){
                     console.log(error);
-                } 
+                    res.send("mala,\n %s",error.toString());
+                }
                 else{
                     console.log('Email enviado a'+correos[i]);
+                    res.send("wena\n");
                 }
             });
         }
@@ -50,7 +54,7 @@ module.exports = function(app,res){
     app.get('/enviar_especifico',function(req, res) {
             var mailOptions = {
                 from: 'no-reply@gojump.cl',
-                to: "benja_as@hotmail.com",
+                to: "alexis.santibanez.14@sansano.usm.cl",
                 subject: 'Mail masivo jump',
                 text: 'Hola, te han invitado a un correo de prueba'
             };
@@ -60,8 +64,8 @@ module.exports = function(app,res){
                     res.send("mala,\n %s",error.toString());
                 }
                 else{
-                    console.log('Email enviado a'+correos[i]);
-                    res.send("wena\n");
+                    console.log('Email enviado al apu');
+                    res.send("wena\n ");
                 }
             });
     });
