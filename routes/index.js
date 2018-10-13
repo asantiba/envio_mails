193a922492d065970a1285f7d2ae2518c286c50f
asantiba <alexis.santibanez.14@sansano.usm.cl>
2018-10-13 17:25:13 -0300

    Enviar correos de a 15 personas listo

--- routes/index.js
+++ routes/index.js
@@ -32,6 +32,7 @@ module.exports = function(app,res){
                     '&lt;/head&gt;&lt;body bgcolor="#FFFFFF" leftmargin="0" topmargin="0" marginwidth="0" marginheight="0"&gt;&lt;img src="cid:unique2@kreata.ee" width="1000" height="1815" alt=""&gt;'+
                     '&lt;/body&gt;&lt;/html&gt;</code></pre></div></body></html>';
         /*
+        //Sacar correos de la base de datos, sin que se repitan
         usuarioModel.get_mails(function(error, data) {
             if(error){
                 console.log(error.message);
@@ -39,47 +40,53 @@ module.exports = function(app,res){
                 correos = data;
             }
         }); */
-        var correos = ['canespfam@gmail.com','acamposa2874@gmail.com','alexis.santibanez.14@sansano.usm.cl'];
+        var correos = ['canespfam@gmail.com','benjamin.meneses.14@sansano.usm.cl'];
         console.log('Cantidad de correos: ' + correos.length);
         var correos_enviados = [];
+        var correos_grupo = [];
         var count = 0;
         for (var i = 0; i < correos.length; i++) {
-            console.log('Enviar email a: ' + correos[i] + '\n');
-            var mailOptions = {
-                from: 'no-reply@gojump.cl',
-                to: correos[i],
-                subject: 'Mail masivo jump',
-                text: 'Hola, te han invitado a un correo de prueba',
-                html: text_html,
-                attachments: [{
-                    filename: 'WEB-copia.png',
-                    path: './public/HTML/01_files/WEB-copia.png',
-                    cid: 'unique@kreata.ee'
-                },
-                {
-                    filename: 'WEB-copia.png',
-                    path: './public/HTML/01_files/WEB-copia.png',
-                    cid: 'unique2@kreata.ee'
-                }]
-            };
-            transporter.sendMail(mailOptions, function(error, info){
-                if(error){
-                    console.log(error);
-                    count = count + 1;
-                    if (count == correos.length) {
-                        res.send('Error, no se han enviado emails');
+            correos_grupo.push(correos[i]);
+            if ((i+1)%15 == 0 || (i+1) == correos.length) {
+                console.log('Enviar email a: ' + correos_grupo);
+                var mailOptions = {
+                    from: 'no-reply@gojump.cl',
+                    to: correos_grupo,
+                    subject: 'Mail  GoJump',
+                    text: 'Saludos, te han invitado a un evento de GoJump',
+                    html: text_html,
+                    attachments: [{
+                        filename: 'WEB-copia.png',
+                        path: './public/HTML/01_files/WEB-copia.png',
+                        cid: 'unique@kreata.ee'
+                    },
+                    {
+                        filename: 'WEB-copia.png',
+                        path: './public/HTML/01_files/WEB-copia.png',
+                        cid: 'unique2@kreata.ee'
+                    }]
+                };
+                transporter.sendMail(mailOptions, function(error, info){
+                    if(error){
+                        console.log(error);
+                        count = count + correos_grupo.length;
+                        if (count == correos.length) {
+                            res.send('Error, no se han enviado emails');
+                        }
                     }
-                }
-                else{
-                    correos_enviados.push(correos[count]);
-                    console.log('Email enviado a ' + correos[count] + ' ' + count);
-                    count = count + 1;
-                    if (count == correos.length) {
-                        res.send('Email enviado a: ' + correos_enviados + '\n');
+                    else{
+                        correos_enviados.push(correos_grupo);
+                        console.log('Email enviado a ' + correos_grupo + ' Cantidad: ' + count);
+                        if (count == correos.length - 1) {
+                            count = count + correos.length%15;
+                            res.send('Email enviado a: ' + correos_enviados + '\n Total: ' + correos_enviados.length);
+                        }
+                        count = count + 4;
                     }
-                }
-            });
-            sleep(3000);
+                });
+                correos_grupo = [];
+                sleep(6000);
+            }
         }
 
     });
